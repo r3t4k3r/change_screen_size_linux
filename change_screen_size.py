57b8878b8a84d7f6b31788280f7455899796b666
r3t4k3r <av.19@list.ru>
2021-08-07 19:38:26 +0300

    some fixes

--- change_screen_size.py
+++ change_screen_size.py
@@ -1,14 +1,14 @@
 #! /usr/bin/env python3
 import subprocess
 
-screen_size = ("1440", "900") # ширина, высота
-screen_name = "DVI-1" # смотри xrandr
-frequency = "60" # частота
+SCREEN_SIZE = ("1440", "900") # ширина, высота // width, height
+SCREEN_NAME = "DVI-1" # смотри xrandr // look xrandr
+FREQUENCY = "60" # частота // frequency
 
-cvt_output = subprocess.check_output(["cvt", screen_size[0], screen_size[1], frequency]).decode()
+cvt_output = subprocess.check_output(["cvt", SCREEN_SIZE[0], SCREEN_SIZE[1], FREQUENCY]).decode()
 newmode = cvt_output.split("Modeline ")[1][:-1]
 newmode_name = newmode.split("\"")[1].split("\"")[0]
 subprocess.call(f"xrandr --newmode {newmode}", shell=True)
-subprocess.call(f"xrandr --addmode {screen_name} {newmode_name}", shell=True)
-subprocess.call(f"xrandr --output {screen_name} --mode {newmode_name}", shell=True)
-print("ok")
+subprocess.call(f"xrandr --addmode {SCREEN_NAME} {newmode_name}", shell=True)
+# comment under line if u want only add screen size without change.
+subprocess.call(f"xrandr --output {SCREEN_NAME} --mode {newmode_name}", shell=True)
